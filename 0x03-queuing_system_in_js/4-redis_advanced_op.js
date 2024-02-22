540a55344b987de02b969e960c319c830c57978f
Koyejo <adinlewakoyejo@yahoo.com>
2024-02-22 13:43:36 +0000

    Add Redis client to store hash value

---
+++ 0x03-queuing_system_in_js/4-redis_advanced_op.js
@@ -0,0 +1,22 @@
+import { createClient, print } from 'redis';
+
+const client = createClient();
+
+client.on('error', err => console.log('Redis client not connected to the server:', err));
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+client.hset('HolbertonSchools', 'Portland', 50, print);
+client.hset('HolbertonSchools', 'Seattle', 80, print);
+client.hset('HolbertonSchools', 'New York', 20, print);
+client.hset('HolbertonSchools', 'Bogota', 20, print);
+client.hset('HolbertonSchools', 'Cali', 50, print);
+client.hset('HolbertonSchools', 'Paris', 2, print);
+
+const getHolbySchools = client.hgetall('HolbertonSchools', (err, reply) => {
+  console.log(reply);
+});
+
+// getHolbySchools('HolbertonSchools');
